0eb5ac02ea78fc72ae74610a2702885b38e756d7
NikitaV <velikihnikita301@gmail.com>
2025-03-05 15:35:41 +0300

    added an image

---
+++ lib/src/screens/custom_login_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final image = AssetImage('assets/images/groupimage.png');
+    return Scaffold(
+        // appBar: AppBar(),
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            SizedBox(height: 40),
+            Center(
+              child: Image(image: image),
+            ),
+            SizedBox(height: 70),
+            //Text(
+            // "Hi, Wecome Back! 👋",
+            // style: TextStyle(
+            //   fontSize: 25,
+            // ),
+            // )
+          ],
+        ));
+  }
+}
