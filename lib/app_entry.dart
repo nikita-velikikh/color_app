e526a50e93c47094b7c97fbe6a6c39d1f28ab78c
NikitaV <velikihnikita301@gmail.com>
2025-03-05 16:00:41 +0300

    change class

--- lib/app_entry.dart
+++ lib/app_entry.dart
@@ -1,4 +1,5 @@
 import 'package:color_aap/src/screens/color_screen.dart';
+import 'package:color_aap/src/screens/custom_login_screen.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ColorScreen(),
+      home: LoginScreen(),
     );
   }
 }
