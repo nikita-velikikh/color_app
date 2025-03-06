9241287e04de661c178a90bb81c17c3fe4025ef7
NikitaV <velikihnikita301@gmail.com>
2025-03-06 10:36:10 +0300

    added text

--- lib/src/screens/custom_login_screen.dart
+++ lib/src/screens/custom_login_screen.dart
@@ -12,21 +12,40 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     final image = AssetImage('assets/images/groupimage.png');
     return Scaffold(
-        appBar: AppBar(toolbarHeight: 51, backgroundColor: Colors.white),
         backgroundColor: Colors.white,
         body: Column(
           children: [
-            SizedBox(height: 40),
+            SizedBox(height: 58),
             Center(
               child: Image(image: image),
             ),
-            SizedBox(height: 70),
-            //Text(
-            // "Hi, Wecome Back! 👋",
-            // style: TextStyle(
-            //   fontSize: 25,
-            // ),
-            // )
+            SizedBox(height: 15),
+            Padding(
+              padding: EdgeInsets.only(left: 29),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Hi, Wecome Back! 👋",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 2),
+            Padding(
+              padding: EdgeInsets.only(left: 31),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Hello again, youve been missed!",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+            ),
           ],
         ));
   }
