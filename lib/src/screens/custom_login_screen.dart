1b356880ee51b6f503554d04e16e1de6132ef220
NikitaV <velikihnikita301@gmail.com>
2025-03-06 17:11:29 +0300

    create password field

--- lib/src/screens/custom_login_screen.dart
+++ lib/src/screens/custom_login_screen.dart
@@ -71,7 +71,44 @@ class EmailField extends StatelessWidget {
           SizedBox(height: 7),
           TextField(
             decoration: InputDecoration(
-              labelText: "Enter your Email",
+              labelText: "Enter Your Email",
+              labelStyle: TextStyle(color: Colors.grey, fontSize: 14),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+                borderSide: BorderSide(
+                  color: Colors.grey,
+                  width: 1.0,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 12),
+          const PasswordField(),
+        ],
+      ),
+    );
+  }
+}
+
+class PasswordField extends StatelessWidget {
+  const PasswordField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //padding: EdgeInsets.only(left: 27, right: 28),
+      alignment: Alignment.centerLeft,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Password",
+            style: TextStyle(fontSize: 14),
+          ),
+          SizedBox(height: 7),
+          TextField(
+            decoration: InputDecoration(
+              labelText: "Enter Your Password",
               labelStyle: TextStyle(color: Colors.grey, fontSize: 14),
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(5),
