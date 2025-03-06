3d33d9e003898212b475bc3d991b9c49978c6263
NikitaV <velikihnikita301@gmail.com>
2025-03-06 13:48:42 +0300

    create email field

--- lib/src/screens/custom_login_screen.dart
+++ lib/src/screens/custom_login_screen.dart
@@ -12,41 +12,78 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     final image = AssetImage('assets/images/groupimage.png');
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(
-          children: [
-            SizedBox(height: 58),
-            Center(
-              child: Image(image: image),
-            ),
-            SizedBox(height: 15),
-            Padding(
-              padding: EdgeInsets.only(left: 29),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Hi, Wecome Back! 👋",
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          SizedBox(height: 58),
+          Center(
+            child: Image(image: image),
+          ),
+          SizedBox(height: 15),
+          Padding(
+            padding: EdgeInsets.only(left: 29),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Hi, Wecome Back! 👋",
+                style: TextStyle(
+                  fontSize: 25,
                 ),
               ),
             ),
-            SizedBox(height: 2),
-            Padding(
-              padding: EdgeInsets.only(left: 31),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Hello again, youve been missed!",
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.grey,
-                  ),
+          ),
+          SizedBox(height: 2),
+          Padding(
+            padding: EdgeInsets.only(left: 31),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Hello again, youve been missed!",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
                 ),
               ),
             ),
-          ],
-        ));
+          ),
+          SizedBox(height: 32),
+          const LoginField(),
+        ],
+      ),
+    );
+  }
+}
+
+class LoginField extends StatelessWidget {
+  const LoginField({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 27, right: 28),
+      alignment: Alignment.centerLeft,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Email",
+            style: TextStyle(fontSize: 14),
+          ),
+          SizedBox(height: 7),
+          TextField(
+            decoration: InputDecoration(
+              labelText: "Enter your Email",
+              labelStyle: TextStyle(color: Colors.grey, fontSize: 14),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+                borderSide: BorderSide(
+                  color: Colors.grey,
+                  width: 1.0,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
