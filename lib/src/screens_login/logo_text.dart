440fff10b3934ce35b7842344524b566c4bebf3a
NikitaV <velikihnikita301@gmail.com>
2025-03-20 08:40:36 +0300

    deleted import

--- lib/src/screens_login/logo_text.dart
+++ lib/src/screens_login/logo_text.dart
@@ -1,6 +1,5 @@
 import 'package:color_aap/src/screens_login/logo_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class LogoText extends StatelessWidget {
   const LogoText({super.key});
