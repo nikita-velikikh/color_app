78f23d7709f274662a153a5033b594d4c7181b73
NikitaV <velikihnikita301@gmail.com>
2025-03-26 16:34:47 +0300

    text

--- lib/src/screens_login/email_field.dart
+++ lib/src/screens_login/email_field.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class EmailField extends StatelessWidget {
   const EmailField({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
