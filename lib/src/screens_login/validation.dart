48b5b0aee9fd153d037e9e3430be7f719c464dc8
NikitaV <velikihnikita301@gmail.com>
2025-04-09 20:58:11 +0300

    added validation

--- lib/src/screens_login/validation.dart
+++ lib/src/screens_login/validation.dart
@@ -1,9 +1,23 @@
-String? validateEmail(String input) {
-  const emailRegex = r'^(?=.*[a-zA-Z])(?=.*[A-Z])(?=.*\d).+$';
-
-  if (RegExp(emailRegex).hasMatch(input)) {
-    return "Invalid Email";
-  } else {
-    return null;
+String? validateEmail(String? input) {
+  final emailRegex = r'^(?=.*[a-zA-Z])(?=.*[A-Z])(?=.*\d).+$';
+  if (input == null) {
+    return "Email is null";
   }
+  final isValidEmail = RegExp(emailRegex).hasMatch(input);
+
+  if (!isValidEmail) {
+    return "Invalid Email";
+  }
+  return null;
+}
+
+String? validatePassword(String? input) {
+  if (input == null || input.isEmpty) {
+    return 'Invalid Password';
+  }
+  const pattern = r'^(?=.*[a-zA-Z])(?=.*[A-Z])(?=.*\d).+$';
+  if (!RegExp(pattern).hasMatch(input)) {
+    return 'Invalid Password';
+  }
+  return null;
 }
