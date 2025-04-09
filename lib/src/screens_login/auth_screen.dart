047268f3c2ca14884b5ad0a1713c538f939bf4ab
NikitaV <velikihnikita301@gmail.com>
2025-04-09 20:56:20 +0300

    added state

--- lib/src/screens_login/auth_screen.dart
+++ lib/src/screens_login/auth_screen.dart
@@ -3,20 +3,38 @@ import 'package:color_aap/src/screens_login/login_form.dart';
 
 import 'package:flutter/material.dart';
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
 
   @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  var emailText = "";
+  var passwordText = "";
+  final formKey = GlobalKey<FormState>();
+  void onLoginPressed() {
+    if (formKey.currentState!.validate()) {
+      print("ok");
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            LoginForm(),
-            LoginButtons(),
+            LoginForm(
+              formKey: formKey,
+            ),
+            LoginButtons(
+              onLoginPressed: onLoginPressed,
+            ),
           ],
         ),
       ),
