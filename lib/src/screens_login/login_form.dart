631af783f3a58aed13381d86c9c76eaf2ac4a498
NikitaV <velikihnikita301@gmail.com>
2025-04-09 20:57:52 +0300

    change state

--- lib/src/screens_login/login_form.dart
+++ lib/src/screens_login/login_form.dart
@@ -1,50 +1,36 @@
 import 'package:color_aap/src/screens_login/custom_field.dart';
 import 'package:color_aap/src/screens_login/logo_text.dart';
 import 'package:color_aap/src/screens_login/password_info.dart';
+import 'package:color_aap/src/screens_login/validation.dart';
 
 import 'package:flutter/material.dart';
 
-class LoginForm extends StatefulWidget {
-  const LoginForm({super.key});
-
-  @override
-  State<LoginForm> createState() => _LoginFormState();
-}
-
-class _LoginFormState extends State<LoginForm> {
-  var emailText = "";
-  var passwordText = "";
-  final _formKey = GlobalKey<FormState>();
+class LoginForm extends StatelessWidget {
+  const LoginForm({super.key, required this.formKey});
+  final GlobalKey formKey;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
         padding: EdgeInsets.only(left: 31),
         child: Form(
-          key: _formKey,
+          key: formKey,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               LogoText(),
               SizedBox(height: 16),
               CustomField(
-                  label: "Email",
-                  hintText: "Enter Your Email",
-                  onChanged: (email) {
-                    setState(() {
-                      emailText = email;
-                    });
-                  }),
+                hintText: "Enter Your Email",
+                onChanged: (email) {},
+                validator: validateEmail,
+              ),
               SizedBox(height: 12),
               CustomField(
-                onChanged: (password) {
-                  setState(() {
-                    passwordText = password;
-                  });
-                },
-                label: "Password",
+                onChanged: (password) {},
                 hintText: "Enter Your Password",
                 isPassword: true,
+                validator: validatePassword,
               ),
               SizedBox(height: 7),
               PasswordInfo(),
