8dbfa45aae07b5a8e64597348477a1c0e800b4b1
NikitaV <velikihnikita301@gmail.com>
2025-04-16 17:01:30 +0300

    setup splash for web and ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:color_aap/app_entry.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const AppEntry());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const AppEntry());
+}
