838fd48ef95c078d781cf8b8bf431ff18932d4e5
AndriiK <andrii.komarychev@gmail.com>
2025-04-16 15:42:40 +0100

    fixed android splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:color_aap/app_entry.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Future.delayed(Duration(seconds: 10));
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(const Duration(seconds: 10));
+  FlutterNativeSplash.remove();
 
   runApp(const AppEntry());
 }
