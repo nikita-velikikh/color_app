e891ba02a8957792380c0e69292b63965508c40a
NikitaV <velikihnikita301@gmail.com>
2025-04-16 17:10:54 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Future.delayed(Duration(seconds: 10));
 
   runApp(const AppEntry());
 }
