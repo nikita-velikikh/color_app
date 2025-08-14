153ae645e32d652eb0aa3d1ba6e587268c6c42ba
Nikita Velikikh <velikikh.nikita@gmail.com>
2025-08-14 19:28:34 +0300

    solved conflict

--- lib/src/screens_login/login_form.dart
+++ lib/src/screens_login/login_form.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+
 import 'package:flutter/material.dart';
 import 'package:color_aap/src/screens_login/custom_field.dart';
 import 'package:color_aap/src/screens_login/logo_text.dart';
