bf6b4ac3d536ab89ee4455a2167d8b840c683862
Nikita Velikikh <velikikh.nikita@gmail.com>
2025-09-08 15:15:28 +0300

    NRS-60: Create file navigation.dart

---
+++ lib/src/services/navigation.dart
@@ -0,0 +1,33 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:color_aap/src/screens/color_screen.dart';
+import 'package:color_aap/src/screens/splash_screen.dart';
+import 'package:color_aap/src/screens_login/auth_screen.dart';
+
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+    AutoRoute(
+      page: SplashRoute.page,
+      path: '/splash',
+      initial: true,
+    ),
+    AutoRoute(
+      page: AuthRoute.page,
+      path: '/auth',
+    ),
+    AutoRoute(
+      page: ColorRoute.page,
+      path: '/color',
+    ),
+  ];
+}
+
+@RoutePage()
+class SplashPage extends SplashScreen {}
+
+@RoutePage()
+class AuthPage extends AuthScreen {}
+
+@RoutePage()
+class ColorPage extends ColorScreen {}
