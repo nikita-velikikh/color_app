67a510c773513a5abacb27778a557c02c3775e36
AndriiK <andrii.komarychev@gmail.com>
2025-10-06 15:59:26 -0300

    NRS-61: used provider to read logic value

--- lib/features/colors/color_screen.dart
+++ lib/features/colors/color_screen.dart
@@ -25,20 +25,8 @@ class ColorScreen extends StatelessWidget {
         builder: (context, colorLogic, child) => GestureDetector(
           onTap: colorLogic.handleBackgroundTap,
           child: Scaffold(
-            appBar: _ColorAppBar(
-              appBarColor: colorLogic.appBarColor,
-              email: email,
-              onTap: () {
-                colorLogic.handleAppBarTap();
-              },
-            ),
-            body: _ColorBody(
-              backgroundColor: colorLogic.backgroundColor,
-              textColor: colorLogic.textColor,
-              counter: colorLogic.counter,
-              onColorButtonPressed: colorLogic.handleButtonTap,
-              onCounterTextTap: () => colorLogic.handleTextTap(),
-            ),
+            appBar: _ColorAppBar(),
+            body: const _ColorBody(),
           ),
         ),
       ),
@@ -48,28 +36,18 @@ class ColorScreen extends StatelessWidget {
 
 /// Custom app bar widget with gesture detection
 class _ColorAppBar extends StatelessWidget implements PreferredSizeWidget {
-  final Color appBarColor;
-  final String email;
-  final VoidCallback onTap;
-
-  const _ColorAppBar({
-    required this.appBarColor,
-    required this.email,
-    required this.onTap,
-  });
-
   @override
   Widget build(BuildContext context) {
     return PreferredSize(
       preferredSize: const Size.fromHeight(60.0),
       child: GestureDetector(
-        onTap: onTap,
+        onTap: context.read<ColorLogic>().handleAppBarTap,
         child: CustomAppBar(
           appBarText: "Color App",
           textColor: Colors.black,
-          backgroundColor: appBarColor,
+          backgroundColor: context.watch<ColorLogic>().appBarColor,
           isCenterTirtle: true,
-          userEmail: email,
+          userEmail: context.watch<ColorLogic>().email,
         ),
       ),
     );
@@ -81,26 +59,14 @@ class _ColorAppBar extends StatelessWidget implements PreferredSizeWidget {
 
 /// Main body widget with animated background
 class _ColorBody extends StatelessWidget {
-  final Color backgroundColor;
-  final Color textColor;
-  final int counter;
-  final Function(Color) onColorButtonPressed;
-  final VoidCallback onCounterTextTap;
-
-  const _ColorBody({
-    required this.backgroundColor,
-    required this.textColor,
-    required this.counter,
-    required this.onColorButtonPressed,
-    required this.onCounterTextTap,
-  });
+  const _ColorBody();
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: AnimatedContainer(
         duration: const Duration(seconds: 1),
-        color: backgroundColor,
+        color: context.watch<ColorLogic>().backgroundColor,
         width: double.infinity,
         height: double.infinity,
         child: Padding(
@@ -108,17 +74,13 @@ class _ColorBody extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              _HeaderText(textColor: textColor),
+              _HeaderText(),
               const SizedBox(height: 20),
-              _InfoText(textColor: textColor),
+              const _InfoText(),
               const SizedBox(height: 40),
-              _ColorButtons(onColorButtonPressed: onColorButtonPressed),
+              const _ColorButtons(),
               const SizedBox(height: 40),
-              _CounterText(
-                counter: counter,
-                textColor: textColor,
-                onTap: onCounterTextTap,
-              ),
+              _CounterText(),
             ],
           ),
         ),
@@ -129,16 +91,12 @@ class _ColorBody extends StatelessWidget {
 
 /// Header text widget
 class _HeaderText extends StatelessWidget {
-  final Color textColor;
-
-  const _HeaderText({required this.textColor});
-
   @override
   Widget build(BuildContext context) {
     return Text(
       S.of(context).hello,
       style: TextStyle(
-        color: textColor,
+        color: context.watch<ColorLogic>().textColor,
         fontSize: 24,
         fontWeight: FontWeight.bold,
       ),
@@ -148,9 +106,7 @@ class _HeaderText extends StatelessWidget {
 
 /// Info text widget with animation
 class _InfoText extends StatelessWidget {
-  final Color textColor;
-
-  const _InfoText({required this.textColor});
+  const _InfoText();
 
   @override
   Widget build(BuildContext context) {
@@ -158,7 +114,10 @@ class _InfoText extends StatelessWidget {
       child: AnimatedDefaultTextStyle(
         duration: const Duration(milliseconds: 500),
         curve: Curves.easeInOut,
-        style: TextStyle(color: textColor, fontSize: 18),
+        style: TextStyle(
+          color: context.watch<ColorLogic>().textColor,
+          fontSize: 18,
+        ),
         child: Text(
           S.of(context).infoColorScreen,
           textAlign: TextAlign.center,
@@ -170,9 +129,7 @@ class _InfoText extends StatelessWidget {
 
 /// Color buttons container widget
 class _ColorButtons extends StatelessWidget {
-  final Function(Color) onColorButtonPressed;
-
-  const _ColorButtons({required this.onColorButtonPressed});
+  const _ColorButtons();
 
   @override
   Widget build(BuildContext context) {
@@ -181,17 +138,14 @@ class _ColorButtons extends StatelessWidget {
         _ColorButton(
           title: S.of(context).blueColorColorScreen,
           color: Colors.blue,
-          onPressed: onColorButtonPressed,
         ),
         _ColorButton(
           title: S.of(context).greenColorColorScreen,
           color: Colors.green,
-          onPressed: onColorButtonPressed,
         ),
         _ColorButton(
           title: S.of(context).redColorColorScreen,
           color: Colors.red,
-          onPressed: onColorButtonPressed,
         ),
       ],
     );
@@ -202,12 +156,10 @@ class _ColorButtons extends StatelessWidget {
 class _ColorButton extends StatelessWidget {
   final String title;
   final Color color;
-  final Function(Color) onPressed;
 
   const _ColorButton({
     required this.title,
     required this.color,
-    required this.onPressed,
   });
 
   static const TextStyle buttonTextStyle = TextStyle(
@@ -224,7 +176,7 @@ class _ColorButton extends StatelessWidget {
         backgroundColor: color,
         minimumSize: buttonSize,
       ),
-      onPressed: () => onPressed(color),
+      onPressed: () => context.read<ColorLogic>().handleButtonTap(color),
       padding: const EdgeInsets.symmetric(vertical: 5.0),
       textStyle: buttonTextStyle,
     );
@@ -233,20 +185,13 @@ class _ColorButton extends StatelessWidget {
 
 /// Counter text widget with gesture detection
 class _CounterText extends StatelessWidget {
-  final int counter;
-  final Color textColor;
-  final VoidCallback onTap;
-
-  const _CounterText({
-    required this.counter,
-    required this.textColor,
-    required this.onTap,
-  });
-
   @override
   Widget build(BuildContext context) {
+    final counter = context.watch<ColorLogic>().counter;
+    final textColor = context.watch<ColorLogic>().textColor;
+
     return GestureDetector(
-      onTap: onTap,
+      onTap: context.read<ColorLogic>().handleTextTap,
       child: Text(
         S.of(context).colorChangedTimes(counter),
         style: TextStyle(color: textColor, fontSize: 18),
